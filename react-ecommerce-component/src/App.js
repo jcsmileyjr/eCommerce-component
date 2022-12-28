8baba204cccbc72a7b24ef141844cf19950b3859
Smiley <jcsmileyjr@gmail.com>
2022-12-28 17:09:38 -0600

    Refactor styles to make plate containers shorter

--- react-ecommerce-component/src/App.js
+++ react-ecommerce-component/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import MenuItem from './components/MenuItem/MenuItem';
 import FrenchFries from './assets/plate__french-fries.png';
+import SalmanPlate from "./assets/plate__salmon-vegetables.png";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <section id="menu">
         <h1>To Go Menu</h1>
         <MenuItem pic={FrenchFries} foodTitle="French Fries with Ketchup" foodPrice="$2.23" />
-        <MenuItem pic={FrenchFries} foodTitle="French Fries with Ketchup" foodPrice="$2.23" />
+        <MenuItem pic={SalmanPlate} foodTitle="Salmon and Vegetables" foodPrice="$5.12" />
       </section>
       <section id="cart">
         <h1>Your Cart</h1>
